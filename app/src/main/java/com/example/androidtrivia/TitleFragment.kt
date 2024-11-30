6086e7b0ba0e981ae3ca7f799e37b348a1b4cbe3
RehanGith <chrehan1007@gmail.com>
2024-11-30 13:53:46 +0700

    Using kotlin extension library feacture to not use random function

--- app/src/main/java/com/example/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/example/androidtrivia/TitleFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.androidtrivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -18,7 +19,7 @@ class TitleFragment : Fragment() {
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener { view: View ->
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
         return binding.root
     }
