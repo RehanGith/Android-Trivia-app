7941f986048500f524a3fbe05be95661b1b596bc
RehanGith <chrehan1007@gmail.com>
2024-11-30 18:30:21 +0700

    Created SafeArgs to send information over to the fragments

--- app/src/main/java/com/example/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/example/androidtrivia/GameWonFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -19,6 +20,8 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener { view: View ->
             Navigation.findNavController(view).popBackStack()
         }
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "numOfQuestion: ${args.numQuestion} , CorrectAnswer: ${args.numCorrect}", Toast.LENGTH_LONG)
         return binding.root
     }
 }
