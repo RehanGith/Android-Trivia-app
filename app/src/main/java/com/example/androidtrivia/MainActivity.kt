0646b43959c657d27b18b0fdd1eb8a15caaf20a4
RehanGith <chrehan1007@gmail.com>
2024-12-01 09:59:56 +0700

    Added DrawrelockMode in mainactivity

--- app/src/main/java/com/example/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.androidtrivia
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -25,6 +27,15 @@ class MainActivity : AppCompatActivity() {
         navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        navController.addOnDestinationChangedListener{nc: NavController, nd:NavDestination,
+        bundle: Bundle? ->
+            if(nd.id == nc.graph.startDestinationId) {
+                drawerLayout.getDrawerLockMode(Gravity.NO_GRAVITY)
+            }else {
+                drawerLayout.getDrawerLockMode(Gravity.LEFT)
+            }
+
+        }
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
